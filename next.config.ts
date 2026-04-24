de5f4bd9fba7057b050bc91e182c3c2876b7b64a
clementmarin <marinclement2002@gmail.com>
2026-04-24 11:32:19 +0200

    deploy: setup fly configuraton and re-enable standalone output

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
   typedRoutes: false,
+  output: "standalone",
 };
 
 export default nextConfig;
